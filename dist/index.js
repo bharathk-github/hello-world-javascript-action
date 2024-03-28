d0a915e42736f32f5584ba51ed7fecbe7b9227ca
Bharath <kb.login@gmail.com>
2024-03-28 19:36:14 +0530

    Dev 1 (#6)
    * add support for propertiesfile
    
    * Update main.yml
    
    * adding ucd properties
    
    * updated workflow to use propertiesfile
    
    * fixed the step id
    
    * prefix file with a forward slash
    
    * chages to github env variable
    
    * point to v1.24 for actions
    
    * fixed incorrect version number in actions checkout
    
    * some changes to requestbody construction
    
    * use v1.25 action
    
    * logging enabled for request data
    
    * testing
    
    * some changes
    
    * mionor improvements
    
    * modified workflow to use 1.27
    
    * disable ssl verification for testing
    
    * dont stringify properties into a json
    
    * Update and rename main.yml to multipleComponents.yml
    
    * Create singleComponent
    
    * Rename singleComponent to singleComponent.yml
    
    ---------
    
    Co-authored-by: bharathk <bharathk@hcl.com>

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,14 @@ module.exports = require("buffer");
 
 /***/ }),
 
+/***/ 147:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs");
+
+/***/ }),
+
 /***/ 687:
 /***/ ((module) => {
 
@@ -267,7 +275,7 @@ var versions;
 var useVersion;
 if (snapshot == null || snapshot == "") {
   try {
-    if(inputVersions == null || inputVersions == ""){
+    if (inputVersions == null || inputVersions == "") {
       console.error('No snapshot (or) Version provided in the input to be deployed ');
       throw new Error("Missing version (or) snapshot in the input");
     }
@@ -282,18 +290,53 @@ if (snapshot == null || snapshot == "") {
   }
 }
 
+const inputPropertiesFile = process.env.INPUT_PROPERTIESFILE;
 const inputProperties = process.env.INPUT_PROPERTIES;
 var properties = null;
-if (inputProperties !== null && inputProperties !== "") {
-  try {
-    properties = JSON.parse(inputProperties);
-  } catch (error) {
-    console.error('Error parsing input properties json ', error)
-    console.error("----------------------------------------")
-    console.error(inputProperties)
-    console.error("----------------------------------------")
-    throw new Error("Acceptable JSON format for properties is {\"prop1\":\"value1\" , \n  \"prop2\":\"value2\" }");
+if (inputPropertiesFile == null || inputPropertiesFile == "") {
+  if (inputProperties == null || inputProperties == "") {
+    console.log('No properties detected in this deployment request');
+  } else {
+    try {
+      properties = JSON.parse(inputProperties);
+    } catch (error) {
+      console.error('Error parsing input properties json ', error)
+      console.error("----------------------------------------")
+      console.error(inputProperties)
+      console.error("----------------------------------------")
+      throw new Error("Acceptable JSON format for properties is {\"prop1\":\"value1\" , \n  \"prop2\":\"value2\" }");
+    }
   }
+} else {
+  const fs = __nccwpck_require__(147);
+  // Read the properties file
+  const propertiesFilePath = process.env.GITHUB_WORKSPACE + inputPropertiesFile;
+
+  console.log("Properties fie path recieved from user " + inputPropertiesFile + "\n and we resolved it to " + propertiesFilePath);
+
+  // Check if the file exists
+  fs.access(propertiesFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('File does not exist!');
+      } else {
+        throw err; 
+      }
+    }
+  });
+
+  const propertiesFileContent = fs.readFileSync(propertiesFilePath, 'utf-8');
+  const props = {};
+  const lines = propertiesFileContent.split('\n');
+  for (const line of lines) {
+    const [key, value] = line.split('=');
+    if (key && value) {
+      props[key.trim()] = value.trim();
+    }
+  }
+  // Convert the properties object to a JSON string
+  properties = props
+
 }
 
 const hostname = process.env.INPUT_HOSTNAME;
@@ -307,16 +350,13 @@ let requestId = '';
 let intervalId;
 const https = __nccwpck_require__(687);
 
-console.error("pass", password)
-console.error("auth", authToken)
-
 let authHeader
-if(authToken !== ""){
+if (authToken !== "") {
   authHeader = `Basic ${Buffer.from(`PasswordIsAuthToken:${authToken}`).toString('base64')}`
-} else if(password !== ""){
+} else if (password !== "") {
   authHeader = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
 } else if (authToken == "" && password == "") {
-   throw new Error("Authentication unsuccessful!, Please provide either UCD password or UCD auth token ");
+  throw new Error("Authentication unsuccessful!, Please provide either UCD password or UCD auth token ");
 }
 
 __nccwpck_require__.e(/* import() */ 960).then(__nccwpck_require__.bind(__nccwpck_require__, 960))
@@ -324,23 +364,24 @@ __nccwpck_require__.e(/* import() */ 960).then(__nccwpck_require__.bind(__nccwpc
     const fetch = module.default;
     const apiUrl = 'https://' + hostname + ':' + port + '/cli/applicationProcessRequest/request';
 
-    const data = {
+    var data = {
       "application": application,
       "applicationProcess": applicationProcess,
       "environment": environment,
       "onlyChanged": onlyChanged,
-      "properties": properties,
       [useVersion ? "versions" : "snapshot"]: useVersion ? versions : snapshot
     };
-
+    
     if (properties !== null) {
+      console.log("adding properties to the request body")
       data.properties = properties;
     }
 
-    console.log("Triggering UCD deployment with " + data);
+
+    console.log("Triggering UCD deployment with " + JSON.stringify(data));
 
     const httpsAgent = new https.Agent({
-      rejectUnauthorized: disableSSLVerification === 'true'
+      rejectUnauthorized: !disableSSLVerification 
     });
 
     fetch(apiUrl, {
@@ -377,7 +418,7 @@ function triggerAPI() {
       const fetch = module.default;
       const apiUrl = 'https://' + hostname + ':' + port + '/cli/applicationProcessRequest/requestStatus?request=' + requestId
       const httpsAgent = new https.Agent({
-        rejectUnauthorized: disableSSLVerification === 'true'
+        rejectUnauthorized: !disableSSLVerification
       });
 
       if (disableSSLVerification === 'true') {
@@ -398,12 +439,12 @@ function triggerAPI() {
           if (result.result === 'SUCCEEDED') {
             console.log('Status is SUCCEEDED. Breaking the loop.');
             clearInterval(intervalId);
-          } else if (['APPROVAL REJECTED','CANCELED','FAILED TO START','FAULTED'].includes(result.result)) {
-            console.error('Deployment failed: status = '+ result.result);
+          } else if (['APPROVAL REJECTED', 'CANCELED', 'FAILED TO START', 'FAULTED'].includes(result.result)) {
+            console.error('Deployment failed: status = ' + result.result);
             clearInterval(intervalId);
             throw new Error('Deployment failed in UCD')
           }
-          
+
         })
         .catch(error => {
           console.error('Error when getting the deployment status of the request:', error);
